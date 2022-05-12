063332dbd9ffedf6722b70cafe7c3be36b134bc2
Styliani Tsovou <stellatsovou@gmail.com>
2022-05-12 17:27:14 +0300

    ex2.6

---
+++ phonebook/src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    const newNumber = {name: newName}
+    console.log('button clicked', event.target)
+    setPersons(persons.concat(newNumber))
+    setNewName('')
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName}
+          onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <Number key={person.name} name={person.name} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+const Number = (props) => {
+  return (
+    <div>
+      <li>{props.name}</li>
+    </div>
+  )
+}
+export default App

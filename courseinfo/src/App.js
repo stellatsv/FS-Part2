d2e01226ebd554f6a6604085125d8580e5a2b9b8
Styliani Tsovou <stellatsovou@gmail.com>
2022-05-12 16:09:02 +0300

    ex2.5

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,3 +1,5 @@
+import Course from './components/Course'
+
 const App = () => {
 	const courses = [
 	  {
@@ -50,51 +52,5 @@ const App = () => {
 		)
 	)
   }
-
-const Course = (props) => {
-	return(
-		<div>
-			<Header course = {props.course.name}/>
-			<Content parts = {props.course.parts}/>
-			<Total parts = {props.course.parts}/>
-		</div>
-	)
-}
-const Header = (props) => {
-	console.log(props)
-	return (<div><h1>{props.course}</h1></div>)
-}
-
-const Content = (props) => {
-	console.log(props)
-	return (<div>
-		{props.parts.map(part => 
-			<Part name={part.name} number={part.exercises}/>
-		)}
-	</div>
-	)
-}
-
-const Total = (props) => {
-	console.log(props)
-	var initialValue = 0
-	const total = 
-  		props.parts.reduce((s, p) => s + p.exercises, initialValue)
-	return (<div>
-		<p>Total of {total} exercises </p>
-	</div>
-	)
-}
-
-const Part = (props) => {
-	console.log(props)
-	return (
-		<div>
-			<p>
-				{props.name} {props.number}
-			</p>
-		</div>
-	) 
-}
 
 export default App
